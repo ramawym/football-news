ae1c6366dd5983a0fdc6f38f02ee2d05986aa253
ramawym <ramadhanmaulana446@gmail.com>
2025-08-27 16:06:43 +0700

    deploy pws

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/walyulahdi.maulana/footballnews"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "walyulahdi-maulana-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
